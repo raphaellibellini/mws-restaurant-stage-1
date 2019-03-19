70de4a8634024489373a0bb049b3552463b503cc
Raphaelli Bellini <rafael.bellini@hotmail.com>
2019-03-19 16:02:16 -0300

    Register Service Worker

--- js/main.js
+++ js/main.js
@@ -4,6 +4,23 @@ let restaurants,
 var newMap
 var markers = []
 
+// registering worker
+registerServiceWorker = () => {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
+}
+
+registerServiceWorker();
+
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
