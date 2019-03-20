3c13426a7e54e7c86839342a0051d601702f453f
Raphaelli Bellini <rafael.bellini@hotmail.com>
2019-03-20 19:00:32 -0300

    Add activate event

--- sw.js
+++ sw.js
@@ -27,6 +27,22 @@ self.addEventListener('install', function(event) {
 });
 
 
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('restaurant-reviews-') &&
+                 cacheName != staticCacheName;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+
 self.addEventListener('fetch', function(event) {
 
   event.respondWith(
